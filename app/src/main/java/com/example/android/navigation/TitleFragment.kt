d3bd0ab216df85b4ffc8b23bc900945e069777b7
Svend_VH <svend.vnhll@eduge.ch>
2023-02-01 09:31:12 +0100

    Point 9 Exercise : Let's Navigate Already

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -13,6 +15,12 @@ class TitleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+        binding.playButton.setOnClickListener{
+            //Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+                view: View ->
+                //    Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+                    view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        }
         return binding.root
     }
 }
